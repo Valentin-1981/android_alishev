1e4b6d951411fa7d16cade72f0ad26a87de5d779
Valentine Chibinyov <chibinyov@rambler.ru>
2023-05-01 21:58:51 +0300

    third commit

--- app/src/main/java/com/testproject/vkinfo/MainActivity.java
+++ app/src/main/java/com/testproject/vkinfo/MainActivity.java
@@ -5,11 +5,16 @@ import static com.testproject.vkinfo.utils.NetworkUtils.getResponseFromURL;
 //import static com.testproject.vkinfo.utils.NetworkUtils;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -19,6 +24,43 @@ public class MainActivity extends AppCompatActivity {
     private EditText searchField;
     private Button searchButton;
     private TextView result;
+
+    class VKQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response = null;
+            try {
+//                    generatedURL = generateURL(searchField.getText().toString());
+                response = getResponseFromURL(urls[0]);
+//                    result.setText(generatedURL.toString());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response){
+            String firstName = null;
+            String lastName = null;
+
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                JSONObject userInfo = jsonArray.getJSONObject(0);
+
+                firstName = userInfo.getString("first_name");
+                lastName = userInfo.getString("last_name");
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+
+            String resultingString = "Имя: " + firstName + "\n" + "Фамилия: " + lastName;
+            result.setText(resultingString);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,20 +74,32 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                URL generatedURL;
-                String response = null;
+                URL generatedURL = null;
                 try {
                     generatedURL = generateURL(searchField.getText().toString());
-                    response = getResponseFromURL(generatedURL);
-                    result.setText(generatedURL.toString());
                 } catch (MalformedURLException e) {
                     throw new RuntimeException(e);
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
-                result.setText(response);
+                new VKQueryTask().execute(generatedURL);
             }
         };
         searchButton.setOnClickListener(onClickListener);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/testproject/vkinfo/utils/NetworkUtils.java
+++ app/src/main/java/com/testproject/vkinfo/utils/NetworkUtils.java
@@ -16,11 +16,16 @@ public class NetworkUtils {
     private static final String PARAM_USER_ID = "user_ids";
     private static final String PARAM_VERSION = "v";
 
+    private static final String ACCESS_TOKEN = "access_token";
+
+
     public static URL generateURL(String userID) throws MalformedURLException {
 
 
         Uri builtUri = Uri.parse(VK_API_BASE_URL + VK_USERS_GET).buildUpon().appendQueryParameter(PARAM_USER_ID, userID)
-                .appendQueryParameter(PARAM_VERSION, "5.8").build();
+                .appendQueryParameter(PARAM_VERSION, "5.89")
+                .appendQueryParameter(ACCESS_TOKEN, "9816b4849816b4849816b4845a9b056201998169816b484fc5956dc6555259d20f3f12e")
+                .build();
 
 //        URL url = null;
         URL url = new URL(builtUri.toString());
